005005708e2321d9b30476245488f824775d9260
getninjaN <getninjan@gmail.com>
2012-01-29 19:25:25 +0100

    Added config. Added reload function.

--- src/se/xtralarge/sol/Sol.java
+++ src/se/xtralarge/sol/Sol.java
@@ -9,6 +9,7 @@ import java.util.Random;
 import java.util.logging.Logger;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.ChatColor;
 
@@ -17,10 +18,17 @@ public class Sol extends JavaPlugin {
 	public static Economy economy = null;
 	private int cost = 50;
 	Logger log = Logger.getLogger("Minecraft"); // Sätt loggningsprylar
+	private FileConfiguration config = null;
 	
 	// När plugin blir aktiverat
 	public void onEnable(){
 		log.info("Sol has been enabled!");
+		
+		this.getConfig().options().copyDefaults(true);
+		saveConfig();
+		
+		config = this.getConfig();
+		cost = config.getInt("cost");
 		
 		setupEconomy();
 	}
@@ -33,6 +41,7 @@ public class Sol extends JavaPlugin {
 	// When player enters command
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
 		Player player = null;
+		double balance = 0;
 		
 		if (sender instanceof Player) {
 			player = (Player) sender;
@@ -41,14 +50,26 @@ public class Sol extends JavaPlugin {
 			return true;
 		}
 		
+		if(player.hasPermission("granis.sol.reload")) {
+			if(args.length == 1) {
+				if(args[0].equalsIgnoreCase("reload")) {
+					configReload(sender);
+					
+					return true;
+				}
+			}
+		}
+		
 		if(cmd.getName().equalsIgnoreCase("sol")) {
 			if(player.hasPermission("granis.sol")) {
 				// Ekonomi grunks
-				double balance = economy.getBalance(player.getName());
-				
-				if (balance < cost) {
-				    player.sendMessage(ChatColor.RED + "- Du har inte råd att köpa sol... Det kostar "+ cost +"c!");;
-				    return true;
+				if(economy != null) {
+					balance = economy.getBalance(player.getName());
+					
+					if (balance < cost) {
+					    player.sendMessage(ChatColor.RED + "- Du har inte råd att köpa sol... Det kostar "+ cost +"c!");;
+					    return true;
+					}
 				}
 				
 				if(player.getWorld().hasStorm()) {
@@ -59,11 +80,14 @@ public class Sol extends JavaPlugin {
 					player.getWorld().setThundering(false);
 					player.getWorld().setWeatherDuration(random.nextInt(168000) + 12000);
 					this.getServer().broadcastMessage(ChatColor.YELLOW +"- Varde ljus! "+ player.getName() + ChatColor.YELLOW +" torkade bort allt regn i "+ player.getWorld().getName() +"!");
-					player.sendMessage("Du är just nu "+ cost +"c riksdaler fattigare, tackar!");
 					
 					// Mer ekonomigrunks
-					economy.withdrawPlayer(player.getName(), cost);
-					economy.depositPlayer("Corningstone", cost);
+					if(economy != null) {
+						player.sendMessage("Du är just nu "+ cost +"c riksdaler fattigare, tackar!");
+						
+						economy.withdrawPlayer(player.getName(), cost);
+						economy.depositPlayer("Corningstone", cost);
+					}
 				} else {
 					player.sendMessage(ChatColor.RED + "- Det regnar ju inte?");
 				}
@@ -85,5 +109,15 @@ public class Sol extends JavaPlugin {
 		}
 		
 		return (economy != null);
+	}
+	
+	private void configReload(CommandSender sender) {
+		this.reloadConfig();
+		
+		config = this.getConfig();
+		cost = config.getInt("cost");
+		
+		sender.sendMessage("Sol config omladdad");
+		sender.sendMessage("Nytt sol-pris - "+ cost +"c");
 	}
 }
